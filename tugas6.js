79a944952ea86e886fcb03a9a53ef5ebe7f98ea3
muhsalfarizi <muhsalfarizi@gmauil.com>
2021-07-13 20:10:03 +0700

    -

---
+++ tugas6.js
@@ -0,0 +1,13 @@
+var nama = prompt("salman");
+var nilai = prompt("Nilai akhir");
+var ket = "";
+
+if(nilai > 70){
+    ket = "Lulus"
+}
+else ket = "Tidak lulus";
+
+Document.write("<p>nama : ${salman}</p> <br>");
+Document.write("<p>nilai : ${Nilai Akhir}</p> <br>");
+Document.write("<p>keterangan : ${ket}</p>");
+
